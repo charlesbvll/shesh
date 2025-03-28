fe8a67d8d80d050717c28b24bdaa174b7257e48c
Charles Beauville <charles@flower.ai>
2025-03-29 00:51:02 +0100

    Write prompt and read input

--- shesh.c
+++ shesh.c
@@ -5,6 +5,18 @@
 int main(int argc, char *argv[]) {
   NOB_UNUSED(argc);
   NOB_UNUSED(argv);
-  printf("Hello world!\n");
+  const char *prompt = "[carlitos@localhost]$ ";
+  write(STDIN_FILENO, prompt, strlen(prompt));
+
+  int i = 0;
+  char *curr = malloc(256);
+  char ch;
+  while (read(STDIN_FILENO, &ch, 1) > 0) {
+    if (i > 255) {
+      return EXIT_SUCCESS;
+    }
+    curr[i] = ch;
+    ++i;
+  }
   return EXIT_SUCCESS;
 }
