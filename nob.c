6390598dcd539321a3a1581939aaa297fbcc5923
Charles Beauville <charles@flower.ai>
2025-03-29 16:01:06 +0100

    Working shell

--- nob.c
+++ nob.c
@@ -5,7 +5,8 @@
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
-  nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", "shesh", "shesh.c");
+  nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-g", "-o", "shesh",
+                 "shesh.c");
   if (!nob_cmd_run_sync(cmd))
     return 1;
   return 0;
--- shesh.c
+++ shesh.c
@@ -11,12 +11,39 @@ int main(int argc, char *argv[]) {
   int i = 0;
   char *curr = malloc(256);
   char ch;
+  Nob_Cmd cmd = {0};
   while (read(STDIN_FILENO, &ch, 1) > 0) {
     if (i > 255) {
-      return EXIT_SUCCESS;
+      break;
     }
-    curr[i] = ch;
-    ++i;
+    if (ch == 'q') {
+      break;
+    }
+
+    if (ch == '\n') {
+      if (strcmp(curr, "exit") == 0) {
+        break;
+      }
+      nob_cmd_append(&cmd, nob_temp_sprintf("%s", curr));
+      nob_cmd_run_sync_redirect(cmd, (Nob_Cmd_Redirect){
+                                         .fdin = STDIN_FILENO,
+                                         .fdout = STDIN_FILENO,
+                                         .fderr = STDIN_FILENO,
+                                     });
+      cmd.count = 0;
+      write(STDIN_FILENO, "\n", 1);
+      write(STDIN_FILENO, prompt, strlen(prompt));
+      memset(curr, 0, 256);
+      i = 0;
+    } else if (ch == ' ') {
+      nob_cmd_append(&cmd, nob_temp_sprintf("%s", curr));
+      memset(curr, 0, 256);
+      i = 0;
+    } else {
+      curr[i] = ch;
+      ++i;
+    }
   }
+  free(curr);
   return EXIT_SUCCESS;
 }
